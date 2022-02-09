75fa70bcbf567fd5199b60353012cfc9b29219ce
Reagan <reaganrandolph1015@gmail.com>
2022-02-09 15:08:16 -0600

    Modified source

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import loadPage from './modules/initial-page';
+
+loadPage();
+/*
+function navEvents() { // allows users to navigate website by clicking tabs
+    // document selectors
+    const homeBtn = document.getElementById('home')
+    const menuBtn = document.getElementById('menu')
+    const contactBtn = document.getElementById('contact')
+    // event listeners
+    homeBtn.addEventListener('click', loadHome)
+    menuBtn.addEventListener('click', loadMenu)
+    contactBtn.addEventListener('click', loadContact)
+}
+*/
+/*
 const content = document.getElementById('content');
 const text = document.createTextNode(
   'This company offers a variety of delicious platters. Entrees and sides are free on orders exceeding $75. We value all customers and employees.'
@@ -13,12 +29,15 @@ content.appendChild(img);
 function textCreate(cont) {
   let textCont = document.createTextNode(cont);
 }
+*/
 
-//function imgCreate(src, alt, title) {
-//  let img = IEWIN ? new Image() : document.createElement('img');
-//  img.src = src;
-//
-//  if (alt != null) img.alt = alt;
-//  if (title != null) img.title = title;
-//  return img;
-//}
+/*
+function imgCreate(src, alt, title) {
+  let img = IEWIN ? new Image() : document.createElement('img');
+  img.src = src;
+
+  if (alt != null) img.alt = alt;
+  if (title != null) img.title = title;
+  return img;
+}
+*/
