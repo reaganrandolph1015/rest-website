e6f563d5e2bc3a9ca7235b85ee40e04f4f4a8db5
Reagan <reaganrandolph1015@gmail.com>
2022-02-09 19:08:29 -0600

    Clean up

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import loadPage from './modules/initial-page';
 import loadHome from './modules/home';
 
 loadPage();
-/*
-function navEvents() { // allows users to navigate website by clicking tabs
-    // document selectors
-    const homeBtn = document.getElementById('home')
-    const menuBtn = document.getElementById('menu')
-    const contactBtn = document.getElementById('contact')
-    // event listeners
-    homeBtn.addEventListener('click', loadHome)
-    menuBtn.addEventListener('click', loadMenu)
-    contactBtn.addEventListener('click', loadContact)
+
+function navEvents() {
+  // allows users to navigate website by clicking tabs
+  // document selectors
+  const homeBtn = document.getElementById('home');
+  const menuBtn = document.getElementById('menu');
+  const contactBtn = document.getElementById('contact');
+  // event listeners
+  homeBtn.addEventListener('click', loadHome);
+  menuBtn.addEventListener('click', loadMenu);
+  contactBtn.addEventListener('click', loadContact);
 }
-*/
