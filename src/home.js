1c12d5d1741f86d513fdfebeb832ba338e8b2233
Reagan <reaganrandolph1015@gmail.com>
2022-02-09 19:03:42 -0600

    Clean up

--- src/home.js
+++
@@ -1,9 +0,0 @@
-const loadPage = () => {
-  let div = document.createElement('div');
-  let title = document.createTextNode(
-    'Hello, I am a node of placeholder text for tab title.'
-  );
-  let h1 = document.createElement('h1');
-  let pageTitle = h1.appendChild(title);
-  div.appendChild(pageTitle);
-};
